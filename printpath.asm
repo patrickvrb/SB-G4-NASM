2c724fa740d09c03fc0be6895544490522973412
hevslc <32377032+hevslc@users.noreply.github.com>
2020-12-02 09:27:24 -0300

    Update printpath.asm

--- printpath.asm
+++ printpath.asm
@@ -2,18 +2,25 @@ global _main
 extern _scanf 
 extern _printf     
 
-segment .data
 
-msg db "Enter your ID", 0xA
-len equ $ - msg
+
+segment .data
+    %defstr path %!PATH         ; The operating system PATH variable
+    pa db path,10
+    palen equ $-pa
+    
 
 segment .bss
 
-id resb 10
 
 segment .text
 
-global main
-main: 
-       push msg
-       call printf
+_main:
+
+       mov eax,pa
+       mov eax,4            ; The system call for write (sys_write)
+       mov ebx,1            ; File descriptor 1 - standard output
+       mov ecx,pa        
+       mov edx,palen    
+       syscall
+       ret  
